3338a901884c1f24faaee4ab0aa710e62d725aad
Shidorien <gaetangerard@hotmail.fr>
2024-09-23 20:39:56 +0200

    update timed button

--- src/renderer/components/shared/bsm-timedboutton/bsm-timed-button.component.tsx
+++ src/renderer/components/shared/bsm-timedboutton/bsm-timed-button.component.tsx
@@ -1,16 +1,48 @@
 import React, { useEffect, MouseEvent, useState } from "react";
 import { useTranslation } from "renderer/hooks/use-translation.hook";
+import { useThemeColor } from "renderer/hooks/use-theme-color.hook";
+import { getCorrectTextColor } from "renderer/helpers/correct-text-color";
+
+type BsmButtonType = "primary" | "secondary" | "none";
 
 type Props = {
     text: string;
+    timeout: number;
     onClick: React.ComponentProps<'button'>['onClick'];
     onComplete: () => void;
+    typeColor?: BsmButtonType;
+
 };
 
-export function BsmTimedButton({ onClick, onComplete, text }: Props) {
+export function BsmTimedButton({ onClick, onComplete, text, timeout, typeColor }: Props) {
     const t = useTranslation();
     const [isVisible, setIsVisible] = useState(false);
     const [timeleft, setTimeleft] = useState(5);
+    const { firstColor, secondColor } = useThemeColor();
+
+    const primaryColor = (() => {
+        if (typeColor === "primary") {
+            return firstColor;
+        }
+        if (typeColor === "secondary") {
+            return secondColor;
+        }
+        return undefined;
+    })();
+
+    const textColor = (() => {
+        if (primaryColor) {
+            return getCorrectTextColor(primaryColor);
+        }
+        return typeColor && typeColor !== "none" ? "white" : undefined;
+    })();
+
+    const renderTypeColor = (() => {
+        if (!typeColor) {
+            return "bg-light-main-color-2 dark:bg-main-color-2"
+        }
+        return "";
+    })();
 
     useEffect(() => {
         setIsVisible(true);
@@ -39,16 +71,21 @@ export function BsmTimedButton({ onClick, onComplete, text }: Props) {
     };
 
     return (
-        <div className="absolute w-full bottom-4 flex-col flex items-center">
-            <span className="z-10 text-white font-bold">{`Download will start in ${timeleft} seconds`}</span>
-            <div className="flex gap-2">
+        <div className="absolute w-full bottom-9 h-8 flex items-center justify-center">
             <button
                 onClick={handleOnClick}
                 type="button"
-                className="px-2 w-fit h-7 flex justify-center items-center rounded-md font-bold bg-light-main-color-2 dark:bg-blue-500 hover:brightness-125 overflow-hidden"
-            >{t(text)}</button>
-            </div>
-            <span className=" text-white text-sm font-bold">3 Versions Selected </span>
+                className={`absolute px-2 w-max h-full flex justify-center items-center rounded-md font-bold ${renderTypeColor} overflow-hidden hover:brightness-125`}
+                style={{ backgroundColor: primaryColor }}
+            >
+                <span className="z-10 text-white" style={{ ...(!!textColor && { color: `${textColor}` }) }}>{t(text)}</span>
+                <div
+                    className={`absolute w-full h-full bg-black opacity-80 transition-transform ease-linear transform ${
+                        isVisible ? 'translate-x-full' : '-translate-x-0'
+                    }`}
+                    style={{ transitionDuration: `${timeout}ms` }}
+                />
+            </button>
         </div>
     );
 }
